133fc9072eb96dee09692030315514a14dd9c510
Ausubel Velasquez <intelectus61@gmail.com>
2022-11-20 23:20:51 -0500

    Anadiendo scrips de python para sacar datos del codigo

--- scritps/scripts_python/script de llenado.py
+++ scritps/scripts_python/script de llenado.py
@@ -1,18 +1,33 @@
-
-# Con Johan
-
-
 print("+++ 2 ++++")
 datos1 = []
 datos2 = []
+datos3 = []
+mensaje = ""
+perfil = ""
+auxiliar = ""+'\r'+'\n'+""
 i = 0
-with open("johan.txt") as fname:
+with open("imecil.txt", encoding="utf8") as fname:
     for lineas in fname:
         datos1.append(lineas.split())
         datos2 = datos1[i]
-        for j in range(0, 3):
-            datos2.pop(0)
-        print(datos2)
+        datos3 = datos2
+        # if ("case" in datos2 ):
+        # print("**************************COMIENZO******")
+        if ("//perfil" in datos2):
+            datos3.pop(0)
+            perfil = " ".join(datos3)
+            mensaje += " \r\n"+perfil+"\r\n"
+
+        if ("string" in datos2):
+            for j in range(0, 3):
+                datos2.pop(0)
+
+            mensaje += " ".join(datos2)
+            mensaje += " "
+            # print(datos2)
         i = i+1
-print(datos2)
-print("***")
+
+print("***NIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+# Aqui se realiza el reemplazo utilizando .replace
+mensaje = mensaje.replace(';', 'auxiliar')
+print(mensaje)
