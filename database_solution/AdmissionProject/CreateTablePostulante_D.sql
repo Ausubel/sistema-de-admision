4fe494ed5ab4f23d7d64be9527f78d449ba78b60
Ausubel Velasquez <intelectus61@gmail.com>
2022-11-27 17:42:17 -0500

    Enviando ultima actualizacion de la base de datos

--- database_solution/AdmissionProject/CreateTablePostulante_D.sql
+++ database_solution/AdmissionProject/CreateTablePostulante_D.sql
@@ -4,9 +4,8 @@ GO
 IF OBJECT_ID('Postulante') IS NULL
 BEGIN
 	CREATE TABLE Postulante(
-		id_postulante INT IDENTITY(20220000,1)NOT NULL,
+		id_postulante INT NOT NULL,
 		id_escuela INT NOT NULL,
-		id_area INT NOT NULL,
 		nombre VARCHAR(100) NOT NULL,
 		apellido_paterno VARCHAR(100) NOT NULL,
 		apellido_materno VARCHAR(100) NOT NULL,
@@ -15,8 +14,6 @@ BEGIN
 		CONSTRAINT PK_postulante PRIMARY KEY (id_postulante),
 		CONSTRAINT FK_estudiante_escuela FOREIGN KEY (id_escuela) REFERENCES
 		Escuela (id_escuela) ON DELETE NO ACTION ON UPDATE NO ACTION,
-		CONSTRAINT FK_estudiante_area FOREIGN KEY (id_area) REFERENCES
-		Area (id_area) ON DELETE NO ACTION ON UPDATE NO ACTION
 	);
 END;
 GO
