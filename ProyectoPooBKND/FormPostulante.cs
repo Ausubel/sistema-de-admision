ba28ea4abd7b5d30a39f70cea1350a42eb568a09
Jacn159 <jcasma37@gmail.com>
2022-11-28 23:54:00 -0500

    Canales no hizo su parte

--- ProyectoPooBKND/FormPostulante.cs
+++ ProyectoPooBKND/FormPostulante.cs
@@ -36,7 +36,7 @@ namespace ProyectoPooBKND
 
         public void llenarTabla()
         {
-            string consulta = "SELECT P.id_postulante AS 'Codigo', CONCAT(P.apellido_paterno,' ',P.apellido_materno,' ',P.nombre) AS 'Postulante', E.nombre_escuela AS 'Carrera', CASE WHEN P.nota IS NULL THEN 'No tiene nota' ELSE CAST(P.nota AS VARCHAR) END AS 'Nota' FROM Postulante P INNER JOIN Escuela E ON P.id_escuela=E.id_escuela; ";
+            string consulta = $"SELECT P.[id_postulante] AS 'CODIGO', P.[nombre] AS 'NOMBRE' , P.[apellido_paterno] AS 'A. PATERNO', P.[apellido_materno] AS 'A. MATERNO' ,P.edad AS 'EDAD' , E.nombre_escuela AS 'CARRERA' FROM [Admission].[dbo].[Postulante] P INNER JOIN Escuela E ON P.id_escuela = E.id_escuela inner join Facultad F on F.id_facultad = E.id_facultad";
             SqlDataAdapter adapter = new SqlDataAdapter(consulta, conn);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
@@ -76,20 +76,46 @@ namespace ProyectoPooBKND
 
         private void buttonEliminar_Click_1(object sender, EventArgs e)
         {
-
+            consultaInput("Delete from postulante where id_postulante = ' " + textBoxCodigoM.Text + "';");
+            MessageBox.Show("REGISTRO ELIMINADO");
+            llenarTabla();
         }
 
         private void buttonModificar_Click(object sender, EventArgs e)
         {
-
+            consultaInput($"UPDATE postulante SET nombre = '{textBoxNombreM.Text}', apellido_paterno = '{textBoxPatM.Text}', apellido_materno = '{textBoxMatM.Text}',edad = {textBoxEdadM.Text} where id_postulante = {textBoxCodigoM.Text}");
+            llenarTabla();
+            vaciarTextboxes();
+            MessageBox.Show("POSTULANTE MODIFICADO CORRECTAMENTE");
         }
-
+        public void vaciarTextboxes()
+        {
+            textBoxNombreM.Clear();
+            textBoxPatM.Clear();
+            textBoxMatM.Clear();
+            textBoxEdadM.Clear();
+            textBoxCarreraM.Clear();
+            textBoxCodigoM.Clear();
+        }
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
 
         private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxCodigoM.Text = dataGridView3.SelectedCells[0].Value.ToString();
+            textBoxNombreM.Text = dataGridView3.SelectedCells[1].Value.ToString();
+            textBoxPatM.Text = dataGridView3.SelectedCells[2].Value.ToString();
+            textBoxMatM.Text = dataGridView3.SelectedCells[3].Value.ToString();
+            textBoxEdadM.Text = dataGridView3.SelectedCells[4].Value.ToString();
+            textBoxCarreraM.Text = dataGridView3.SelectedCells[5].Value.ToString();
+
+            //Deshabilito caja de texto carrera
+            textBoxCarreraM.Enabled = false;
+        }
+
+        private void groupBox10_Enter_1(object sender, EventArgs e)
         {
 
         }
